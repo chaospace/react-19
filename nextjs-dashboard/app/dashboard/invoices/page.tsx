8364a2bb8286426672b428577ef64bd3750b8bf7
chaospace <ichaospace@gmail.com>
2025-02-25 15:23:37 +0900

    [DASHBOARD] routeGroup를 이용한 스켈레톤 적용

--- nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -1,6 +1,8 @@
+import { fetchCardData } from "@/app/lib/data"
 
 
 
-export default function Page() {
+export default async function Page() {
+  const { totalPaidInvoices } = await fetchCardData();
   return <p>Invoices Page</p>
 }
