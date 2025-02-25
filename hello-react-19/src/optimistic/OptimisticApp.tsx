9a1675e399813c46325c26dec8f7e88c882599c6
chaospace <ichaospace@gmail.com>
2025-02-25 12:00:07 +0900

    [REACT-19] useOptimistic 동작 스터디

---
+++ hello-react-19/src/optimistic/OptimisticApp.tsx
@@ -0,0 +1,62 @@
+import { useOptimistic, useRef, useState } from "react";
+
+type MessageVO = {
+  text: string;
+  sending: boolean;
+  key: string | number
+}
+
+const Thread = ({ messages, sendMessage }: any) => {
+  const formRef = useRef<HTMLFormElement>(null);
+  const [optimisticMessages, addOptimisticMessage] = useOptimistic(messages, (state, newMessage) => {
+    return [...state, {
+      text: newMessage,
+      sending: true
+    }]
+  });
+
+  // api 요청 전 message에 pending상태를 추가하는 optimistic Hook을 이용한다.
+  async function formAction(formData: FormData) {
+    addOptimisticMessage(formData.get('message'));
+    formRef.current?.reset();
+    await sendMessage(formData);
+  }
+  return (
+    <>
+      {
+        optimisticMessages.map((message: MessageVO, index: number) => (
+          <div key={ index }>
+            { message.text }
+            { !!message.sending && (<small>(...sending)</small>) }
+          </div>)
+        ) }
+      <form ref={ formRef } action={ formAction }>
+        <input type="text" name="message" placeholder="hello" />
+        <button type="submit">Send</button>
+      </form>
+    </>
+  )
+}
+
+const deliverMessage = async (message: string) => {
+  await new Promise(res => setTimeout(res, 1000))
+  return message;
+}
+
+function OptimisticApp() {
+  const [messages, setMessages] = useState<MessageVO[]>([
+    { text: 'Hello there!', sending: false, key: 1 }
+  ]);
+
+  //api 요청 처리 액션
+  async function sendMessage(formData: FormData) {
+    const sentMessage = await deliverMessage((formData?.get('message') || '') as string);
+    setMessages(messages => [...messages, { text: sentMessage, sending: false, key: messages.length + 1 }]);
+  }
+  return (
+    <Thread messages={ messages } sendMessage={ sendMessage } />
+  )
+}
+
+
+export default OptimisticApp;
