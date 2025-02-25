81644e15d569198c92cc12b4150f1c1f06e834be
chaospace <ichaospace@gmail.com>
2025-02-25 11:59:00 +0900

    [REACT-19] 기본환경 구성

---
+++ hello-react-19/src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import FormStatus from './formStatus/FromStatus';
+import OptimisticApp from './optimistic/OptimisticApp';
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      {/* <FormStatus /> */ }
+      <OptimisticApp />
+    </div>
+  );
+}
+
+export default App;
