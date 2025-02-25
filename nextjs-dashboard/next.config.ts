d4d114bdab413ebe7736a191e5aeedc96c142c1c
chaospace <ichaospace@gmail.com>
2025-02-25 16:15:45 +0900

    [DASHBOARD] ppr옵션을 이용한 부분 렌더링 적용( 실험기능 )

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  experimental: {
+    ppr: true
+  },
   /* config options here */
   webpack: (config) => {
     config.externals = [...config.externals, 'bcrypt'];
