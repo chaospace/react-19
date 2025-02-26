728fc0b8783f1f12708186d491794ca13adfed5d
chaospace <ichaospace@gmail.com>
2025-02-26 12:54:13 +0900

    [CHAPTER12] zod를 이용한 데이터 타입 검증 및  생성 후 리다이렉트 적용

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -1,15 +1,40 @@
 'use server';
+
+
+import { z } from "zod";
+import postgres from "postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'verify-full' });
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string()
+})
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 /**
  * ? 서버액션이라 브라우저에서는 log가 안보임 terminal에서 확인
  * @param formData 
  */
 async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status')
-  }
-  console.log('rawData', rawFormData);
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+  //db에 데이터 쓰기
+  await sql`INSERT INTO invoices (customer_id, amount, status, date) VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+  //패스 유효성 검증(캐시데이터 갱신) 후 리다이렉트
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
 
 
