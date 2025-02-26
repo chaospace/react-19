c87f1dfddf9cd5f3d779459bb296e475fff7c412
chaospace <ichaospace@gmail.com>
2025-02-26 17:50:30 +0900

    [CHAPTER13] 생성 시 폼 유효성 처리 추가

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -10,16 +10,29 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'verify-full' });
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.'
+  }),
+  amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater then $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.'
+  }),
   date: z.string()
 })
+
+type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null
+}
 
 const InvoiceDAO = FormSchema.omit({ id: true, date: true });
 
 const getInvoiceData = (formData: FormData) => {
-  return InvoiceDAO.parse({
+  return InvoiceDAO.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status')
@@ -30,8 +43,16 @@ const getInvoiceData = (formData: FormData) => {
  * ? 서버액션이라 브라우저에서는 log가 안보임 terminal에서 확인
  * @param formData 
  */
-async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = getInvoiceData(formData);
+async function createInvoice(prevState: State, formData: FormData) {
+  const validateFields = getInvoiceData(formData);
+  console.log('create', validateFields)
+  if (!validateFields.success) {
+    return {
+      errors: validateFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice'
+    }
+  }
+  const { customerId, amount, status } = validateFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
   try {
@@ -40,6 +61,9 @@ async function createInvoice(formData: FormData) {
   } catch (error) {
     // 콘솔을 통해 에러 확인
     console.error(error);
+    return {
+      message: 'Database Error: Failed to Create Invoice'
+    };
   }
   //패스 유효성 검증(캐시데이터 갱신) 후 리다이렉트
   revalidatePath('/dashboard/invoices');
@@ -47,12 +71,22 @@ async function createInvoice(formData: FormData) {
 }
 
 async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = getInvoiceData(formData);
+  const validateFields = getInvoiceData(formData);
+  if (!validateFields.success) {
+    return {
+      errors: validateFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice'
+    }
+  }
+  const { customerId, amount, status } = validateFields.data;
   const amountInCents = amount * 100;
   try {
     await sql`UPDATE invoices SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status} WHERE id = ${id}`;
   } catch (error) {
     console.error(error);
+    return {
+      message: 'Database Error: Failed to Update Invoice'
+    }
   }
 
   revalidatePath('/dashboard/invoices');
@@ -71,7 +105,7 @@ async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
-
+export type { State };
 export {
   createInvoice,
   updateInvoice,
