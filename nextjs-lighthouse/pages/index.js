0fcc2686c8d7bb2f6c9d7a1a47b77fd3a338a747
chaospace <ichaospace@gmail.com>
2025-02-27 18:23:22 +0900

    [LIGHTHOUSE] next/image를 이용한 이미지 최적화 적용

--- nextjs-lighthouse/pages/index.js
+++ nextjs-lighthouse/pages/index.js
@@ -1,18 +1,19 @@
-import { useState } from 'react';
-import Head from 'next/head';
+import { useState } from "react";
+import Head from "next/head";
 
-import Fuse from 'fuse.js';
-import _ from 'lodash';
+import Fuse from "fuse.js";
+import _ from "lodash";
 
-import { countries } from '../countries';
-import styles from '../styles/Home.module.css';
-import CodeSampleModal from '../components/CodeSampleModal';
+import { countries } from "../countries";
+import styles from "../styles/Home.module.css";
+import CodeSampleModal from "../components/CodeSampleModal";
+import Image from "next/image";
 
 export default function Start({ countries }) {
   const [results, setResults] = useState(countries);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const fuse = new Fuse(countries, {
-    keys: ['name'],
+    keys: ["name"],
     threshold: 0.3,
   });
 
@@ -34,7 +35,12 @@ export default function Start({ countries }) {
         </h1>
 
         <div className={styles.heroImage}>
-          <img src="large-image.jpg" alt="Large Image" />
+          <Image
+            src="/large-image.jpg"
+            alt="Large Image"
+            width={3048}
+            height={2024}
+          />
         </div>
 
         <div>
@@ -92,7 +98,7 @@ export default function Start({ countries }) {
         >
           Powered by
           <span className={styles.logo}>
-            <img src="/vercel.svg" alt="Vercel Logo" />
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
